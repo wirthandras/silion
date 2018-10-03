3820c5641bd2e9eafb6a6731afdcee9ef40eb4f7
Andras Wirth <agrippa.wia@gmail.com>
2018-10-03 08:27:47 +0200

    NewController simplified

--- src/main/java/hu/wirthandras/silion/controller/NewController.java
+++ src/main/java/hu/wirthandras/silion/controller/NewController.java
@@ -2,10 +2,9 @@ package hu.wirthandras.silion.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import hu.wirthandras.silion.domain.NewTranslation;
 import hu.wirthandras.silion.service.TranslationServiceInterface;
@@ -16,13 +15,12 @@ public class NewController {
 	@Autowired
 	private TranslationServiceInterface translationSevice;
 	
-	@RequestMapping("new")
-	public String newTranslation(Model model) {
-		model.addAttribute("object", new NewTranslation());
+	@GetMapping("/new")
+	public String newTranslation(@ModelAttribute("object") NewTranslation t) {
 		return "new";
 	}
 	
-	@RequestMapping(value = "/newelement", method = RequestMethod.POST)
+	@PostMapping("/new")
 	public String newTranslationSave(@ModelAttribute NewTranslation object) {
 		translationSevice.save(object.getBase(), object.getTranslation());
 		return "redirect:random";
